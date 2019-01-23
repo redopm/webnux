dd68bd475613c3d9f3740fcbdf20da2234fb2f23
redopm <omprakashmaury001#gmail.com>
2019-01-23 14:20:37 +0530

    twitter data scrping

--- link.py
+++ link.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python2
 import  urllib2
 import  googlesearch  
-#
-a=raw_input("Search here... :")
+import sys
+a=sys.argv[0]
 webdata=googlesearch.search(a,num=1)
 f=open('link.txt','w+')# open a file link.text and it exist then create write mod
 for  i  in   webdata:
-	print  i
+	#print  i
 	f.write("%s\n" %i) # this is the write operation in file
 	f1=f.readline()
 	for x in  f1:
